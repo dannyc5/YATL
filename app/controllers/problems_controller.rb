8996214877d859636f51646c6ff79896d88efe13
Daniel Chang <daniel.chang85@gmail.com>
2014-04-20 17:20:05 -0400

    COMMIT 3: Iteration 1. Example notes-- create problem

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -4,4 +4,14 @@ class ProblemsController < ApplicationController
     @problems = Problem.all
   end
 
+  def create
+    Problem.create!(problem_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def problem_params
+    params.require(:problem).permit(:message)
+  end
 end
