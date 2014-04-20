86286cfda0a79fe039fe4f9885baf619b69e1b54
Daniel Chang <daniel.chang85@gmail.com>
2014-04-20 17:20:05 -0400

    COMMIT 1: Set up testing environment

---
+++ features/step_definitions/create_a_problem_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the problems page$/) do
+  visit root_path
+end
+
+When(/^I click add problem$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill out a new problem$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the new problem$/) do
+  pending # express the regexp above with the code you wish you had
+end
